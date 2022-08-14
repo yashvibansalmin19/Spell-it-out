589233f6776e85465e10364d4b06369bad26a69b
yashvi_bansal <yashvi.bansal.min19@itbhu.ac.in>
2022-08-14 18:24:34 +0530

    New

--- app.js
+++ app.js
@@ -44,3 +44,21 @@ document.querySelector("#submit").addEventListener("click", () => {
     }
 
 });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+    database: "mydb"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
